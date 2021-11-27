ad1cd380895997e5ca14108750d88f17512a3f36
Jungsoo Kim <hollibleling12@gmail.com>
2021-11-27 21:01:42 +0900

    feat: test code

--- cardoc/user/models.py
+++ cardoc/user/models.py
@@ -2,12 +2,21 @@ from django.contrib.auth.hashers import make_password
 from django.db import models
 from django.contrib.auth.base_user import AbstractBaseUser
 from django.contrib.auth.models import UserManager
+from django.contrib.auth.password_validation import validate_password
+
+from rest_framework.validators import ValidationError
 
 
 class CustomUserManager(UserManager):
     def _create_user(self, email, password, **extra_fields):
         email = self.normalize_email(email)
         user = self.model(email=email, **extra_fields)
+        try:
+            if validate_password(password, user=user):
+                return password
+        except:
+            raise ValidationError("incorrect validation")
+        
         user.password = make_password(password)
         user.save(using=self._db)
         return user
@@ -22,4 +31,3 @@ class User(AbstractBaseUser):
 
     USERNAME_FIELD = 'email'
     objects = CustomUserManager()
-    
